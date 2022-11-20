af9e2dca11fb8190c0aac605980f3466f5a321b3
Alex Zaitsev <saizewalex@gmail.com>
2022-11-20 21:41:12 +0300

    Renaming variables, Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,7 @@
 public class BmiService {
+    public double calculate(double weight_kg, double height_m) {
 
-    public double calculate(double m, double h) {
-
-        double result = m / (h * h);
+        double result = weight_kg / (height_m * height_m);
         return result;
-
-            }
-
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
-
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-        double bmi = service.calculate(96.564, 1.76);
+        double bmi = service.calculate(86.534, 1.80);
+
         System.out.println("Индекс массы тела равен:");
         System.out.printf("%.2f", bmi);
-
     }
 }
