0659a90589ad76b03d7c72644a7bb259d91614be
Alex Zaitsev <saizewalex@gmail.com>
2022-11-25 21:37:58 +0300

    Renaming variables #2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public double calculate(double weight_kg, double height_m) {
+    public double calculate(double weightKg, double heightM) {
 
-        double result = weight_kg / (height_m * height_m);
+        double result = weightKg / (heightM * heightM);
         return result;
     }
 }
